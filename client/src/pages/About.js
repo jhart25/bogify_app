904332da93497b550768f07c7d15152081db5545
Andrew Violan <andrew.violan@gmail.com>
2019-03-09 12:40:04 -0800

    login working

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,21 +1,62 @@
-import React from "react";
-import Hero from "../components/Hero";
-import Container from "../components/Container";
-import Row from "../components/Row";
-import Col from "../components/Col";
+import React, { Component } from "react";
+import Spotify from 'spotify-web-api-js';
 
-function About() {
-  return (
-    <div>
-      <Hero backgroundImage="">
-        <h1>Bogify</h1>
-        <h2>A more Social Spotify</h2>
-        <a href="http://localhost:8888">
-          <button>Login With Spotify</button>
+const spotifyWebApi = new Spotify();
+
+class About extends Component {
+  constructor(){
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyWebApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: 'Not Checked', albumArt: '' }
+    }
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+       e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying(){
+    spotifyWebApi.getMyCurrentPlaybackState()
+      .then((response) => {
+        this.setState({
+          nowPlaying: { 
+              name: response.item.name, 
+              albumArt: response.item.album.images[0].url
+            }
+        });
+      })
+  }
+  render() {
+    return (
+      <div className="App">
+        <a href='http://localhost:8888'> 
+          <button>Login to Spotify</button> 
         </a>
-      </Hero>
-    </div>
-  );
+        <div>
+          Now Playing: { this.state.nowPlaying.name }
+        </div>
+        <div>
+          <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }}/>
+        </div>
+          <button onClick={() => this.getNowPlaying()}>
+            Check Now Playing
+          </button>
+      </div>
+    );
+  }
 }
 
 export default About;
