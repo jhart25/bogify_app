2b3e766b56a76ca72f454c6e51282c32c1e78dee
Andrew Violan <andrew.violan@gmail.com>
2019-03-09 10:44:06 -0800

    login page

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -3,19 +3,38 @@ import API from "../utils/API";
 import Container from "../components/Container";
 import SearchForm from "../components/SearchForm";
 import SearchResults from "../components/SearchResults";
+import Spotify from 'spotify-web-api-js';
+
+const spotifyWebApi = new Spotify();
 
 class Search extends Component {
-  state = {
-    search: "",
-    tracks: [],
-    results: [],
-    error: ""
-  };
+  constructor(){
+    super();
+    const params = this.getHashParams();
+    this.state ={
+      loggedIn: params.access_token ? true : false,
+      search: "",
+      artists: [],
+      results: [],
+      error: ""
+    }
+    if (params.access_token){
+      spotifyWebApi.setAccessToken(params.access_token)
+    }
+  }
+   getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
 
-  // When the component mounts, get a list of tracks
   componentDidMount() {
-    API.getTrack()
-      .then(res => this.setState({ tracks: res.data.message }))
+    API.getArtist()
+      .then(res => this.setState({ artists: res.data.message }))
       .catch(err => console.log(err));
   }
 
@@ -25,7 +44,7 @@ class Search extends Component {
 
   handleFormSubmit = event => {
     event.preventDefault();
-    API.getTrack(this.state.search)
+    API.getArtist(this.state.search)
       .then(res => {
         if (res.data.status === "error") {
           throw new Error(res.data.message);
@@ -34,15 +53,16 @@ class Search extends Component {
       })
       .catch(err => this.setState({ error: err.message }));
   };
+
   render() {
     return (
       <div>
         <Container style={{ minHeight: "80%" }}>
-          <h1 className="text-center">Search for tracks</h1>
+          <h1 className="text-center">Search for artists</h1>
           <SearchForm
             handleFormSubmit={this.handleFormSubmit}
             handleInputChange={this.handleInputChange}
-            tracks={this.state.tracks}
+            artists={this.state.artists}
           />
           <SearchResults results={this.state.results} />
         </Container>
