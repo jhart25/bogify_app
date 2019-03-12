82556e72d026d48b966ba3315914580613050748
Andrew Violan <andrew.violan@gmail.com>
2019-03-11 20:20:56 -0700

    bootstrap and pages modified

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,74 +1,31 @@
 import React, { Component } from "react";
-import API from "../utils/API";
 import Container from "../components/Container";
-import SearchForm from "../components/SearchForm";
-import SearchResults from "../components/SearchResults";
-import Spotify from 'spotify-web-api-js';
-
-const spotifyWebApi = new Spotify();
+import { SpotifyApiContext, Artist } from 'react-spotify-api';
 
 class Search extends Component {
-  constructor(){
-    super();
-    const params = this.getHashParams();
-    this.state ={
-      loggedIn: params.access_token ? true : false,
-      search: "",
-      artists: [],
-      results: [],
-      error: ""
-    }
-    if (params.access_token){
-      spotifyWebApi.setAccessToken(params.access_token)
-    }
-  }
-   getHashParams() {
-    var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    while ( e = r.exec(q)) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-    }
-    return hashParams;
-  }
 
-  componentDidMount() {
-    API.getArtist()
-      .then(res => this.setState({ artists: res.data.message }))
-      .catch(err => console.log(err));
-  }
-
-  handleInputChange = event => {
-    this.setState({ search: event.target.value });
-  };
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-    API.getArtist(this.state.search)
-      .then(res => {
-        if (res.data.status === "error") {
-          throw new Error(res.data.message);
-        }
-        this.setState({ results: res.data.message, error: "" });
-      })
-      .catch(err => this.setState({ error: err.message }));
-  };
-
-  render() {
-    return (
-      <div>
-        <Container style={{ minHeight: "80%" }}>
-          <h1 className="text-center">Search for artists</h1>
-          <SearchForm
-            handleFormSubmit={this.handleFormSubmit}
-            handleInputChange={this.handleInputChange}
-            artists={this.state.artists}
-          />
-          <SearchResults results={this.state.results} />
-        </Container>
-      </div>
-    );
-  }
+render(props) {
+  return (
+    <Container>
+      <SpotifyApiContext.Provider value={props.token}>
+          <Artist id={props.id}>
+              {(artist, loading, error) =>
+                  artist ? (
+                      <div>
+                          <h1>{artist.name}</h1>
+                          <ul>
+                              {artist.genres.map(genre => (
+                              <li key={genre}>{genre}</li>
+                              ))}
+                          </ul>
+                      </div>
+                  ) : null
+              }
+          </Artist>
+      </SpotifyApiContext.Provider>
+    </Container>
+  )
+}
 }
 
 export default Search;
